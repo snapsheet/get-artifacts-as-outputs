d0b449900b913f811103eea4e00345a7bde6cec7
David Tittle <david.tittle@snapsheet.me>
2024-05-17 12:34:29 -0500

    Add action file and logic for fetching Artifacts and presenting them as Outputs.

--- __tests__/consolidator.test.ts
+++ __tests__/consolidator.test.ts
@@ -1,7 +1,19 @@
 import { ContextFactory } from "./factories/github/context";
 import { Consolidator } from "../src/consolidator";
+import { GetContentResponseFactory } from "./factories/octokit/getContentResponse";
+import { ResponseFactory } from "./factories/response";
+import { ListWorkflowRunArtifactResponseFactory } from "./factories/octokit/listWorkflowRunArtifactResponse";
+import { WorkflowContentFactory } from "./factories/workflowContent";
+import { ListJobsForWorkflowRunFactory } from "./factories/octokit/listJobsForWorkflowRunResponse";
+import { WorkflowJobFactory } from "./factories/octokit/workflowJob";
+import { DownloadArtifactResponseFactory } from "./factories/octokit/downloadArtifactResponse";
+import core from "@actions/core";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
+import fs from "fs";
+import unzipper from "unzipper";
+import { ArtifactInfo } from "../src/artifactInfo";
+import { JobInfo } from "../src/jobInfo";
 
 /**
  * Mock the `core` logging functions so they don't show in test cases.
@@ -18,6 +30,8 @@ jest.mock<typeof import("@actions/core")>("@actions/core", () => {
   };
 });
 
+const workflowContent = WorkflowContentFactory.generate();
+
 /**
  * Mock the `github` module environment data and API endpoints for test cases.
  */
@@ -27,7 +41,51 @@ jest.mock<typeof import("@actions/github")>("@actions/github", () => {
 
   return {
     ...originalGithub,
-    context: ContextFactory.generate()
+    context: ContextFactory.generate(),
+    getOctokit: jest.fn(() => {
+      return originalGithub.getOctokit("no_token", {
+        throttle: { enabled: false },
+        request: {
+          fetch: jest.fn(async (endpoint) => {
+            let response: ResponseFactory | null = null;
+
+            if (/\/contents\//.test(endpoint)) {
+              response = GetContentResponseFactory.generate({
+                url: endpoint,
+                data: {
+                  content: Buffer.from(
+                    JSON.stringify(workflowContent),
+                    "utf-8"
+                  ).toString("base64")
+                }
+              });
+            } else if (/\/artifacts$/.test(endpoint)) {
+              response = ListWorkflowRunArtifactResponseFactory.generate({
+                url: endpoint
+              });
+            } else if (/\/actions\/runs\/\d+\/jobs$/.test(endpoint)) {
+              response = ListJobsForWorkflowRunFactory.generate({
+                url: endpoint
+              });
+            } else if (
+              /\/actions\/runs\/\d+\/attempts\/\d+\/jobs$/.test(endpoint)
+            ) {
+              response = ListJobsForWorkflowRunFactory.generate({
+                url: endpoint
+              });
+            } else if (
+              /\/actions\/artifacts\/\d+\/zip$/.test(endpoint)
+            ) {
+              response = DownloadArtifactResponseFactory.generate({
+                url: endpoint
+              });
+            }
+
+            return response;
+          })
+        }
+      });
+    })
   };
 });
 
@@ -41,6 +99,20 @@ jest.mock<typeof import("../src/consolidator")>("../src/consolidator", () => {
   >("../src/consolidator");
   return {
     ...originalConsolidator
+  };
+});
+
+/**
+ * Mock the `fs` module method that writes to files.
+ */
+jest.mock<typeof import("fs")>("fs", () => {
+  const originalFs = jest.requireActual<typeof import("fs")>("fs");
+
+  return {
+    ...originalFs,
+    createWriteStream: jest.fn(() => {
+      return originalFs.createWriteStream("/dev/null");
+    })
   };
 });
 
@@ -62,5 +134,282 @@ describe("Consolidator", () => {
 
   it("should be defined", () => {
     expect(subject).toBeDefined();
+  });
+
+  describe("commonQueryParams", () => {
+    it("shows expected common query params", async () => {
+      expect(subject.commonQueryParams()).toEqual({
+        owner: subject.context.payload.organization.login,
+        repo: subject.context.payload.repository?.name
+      });
+    });
+  });
+
+  describe("getWorkflowSchema", () => {
+    it("queries and parses the workflow file from the content API", async () => {
+      expect(await subject.getWorkflowSchema()).toEqual(workflowContent);
+    });
+  });
+
+  describe("getLastRanWorkflowJobs", () => {
+    let jobs: JobInfo[];
+    let originalJobs: JobInfo[];
+    let skippedJobs: JobInfo[];
+    let methodSpy: jest.SpyInstance;
+    
+    beforeEach(() => {
+      const mockJobsResponse = ListJobsForWorkflowRunFactory.generate();
+      jobs = mockJobsResponse.data.jobs as JobInfo[];
+      originalJobs = jobs.map((j) => { return {...j}; }); // duplicate for test assertions
+      skippedJobs = jobs.slice(-5);
+      const origSkippedInfo = skippedJobs.map((j) => { return {...j}; });
+
+      // Set 5 of the jobs attributes to look like a rerun occurred.
+      skippedJobs.forEach((job) => {
+        job.runner_id = 0;
+        job.runner_name = "";
+        job.runner_group_id = 0;
+        job.runner_group_name = "";
+      });
+      methodSpy = jest.spyOn(subject, "getRelevantWorkflowJobs");
+      methodSpy.mockImplementation(async (jobName: string, runId: number, runAttempt?: number | null) => {
+        jobName; runId; runAttempt; // for validation...
+        
+        return origSkippedInfo;
+      });
+    });
+
+    it("returns the job set that was passed in and makes no queries if no jobs were skipped", async () => {
+      // set original values back for skipped jobs
+      skippedJobs.forEach((job) => {
+        const originalJob = originalJobs.find((oj) => oj.id = job.id);
+        job.runner_id = originalJob?.runner_id || 5; // adding 5/"something" so that TypeScript checks pass
+        job.runner_name = originalJob?.runner_name || "something";
+        job.runner_group_id = originalJob?.runner_group_id || 5;
+        job.runner_group_name = originalJob?.runner_group_name || "something";
+      });
+
+      const result = await subject.getLastRanWorkflowJobs("doesnt_matter", jobs);
+      expect(result).toEqual(jobs);
+      expect(methodSpy).not.toHaveBeenCalled();
+    });
+
+    it("retries when it finds jobs with run_attempt of zero", async () => {
+      const result = await subject.getLastRanWorkflowJobs("doesnt_matter", jobs);
+      expect(result).toEqual(originalJobs);
+      const runAttempt = (originalJobs[0].run_attempt || 1);
+      expect(methodSpy).toHaveBeenCalledWith("doesnt_matter", subject.context.runId, runAttempt - 1);
+    });
+
+    it("returns empty results when passed jobs are empty", async () => {
+      const result = await subject.getLastRanWorkflowJobs("doesnt_matter", []);
+      expect(result).toEqual([]);
+    });
+
+    it("uses default of one if there is no run_attempt attribute, making no additional queries", async () => {
+      jobs.forEach((job) => delete job.run_attempt);
+      await subject.getLastRanWorkflowJobs("doesnt_matter", jobs);
+      expect(methodSpy).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("getRelevantWorkflowJobs", () => {
+    it("awaits the query and calls the method to filter results", async () => {
+      const spy1 = jest.spyOn(subject, "getWorkflowJobs").mockImplementation();
+      const spy2 = jest
+        .spyOn(subject, "filterForRelevantJobDetails")
+        .mockImplementation();
+
+      await subject.getRelevantWorkflowJobs("someJob", 1234, 3);
+
+      expect(spy1).toHaveBeenCalled();
+      expect(spy2).toHaveBeenCalled();
+    });
+  });
+
+  describe("getWorkflowJobs", () => {
+    it("fetches workflow jobs if there were no reruns", async () => {
+      expect((await subject.getWorkflowJobs(12)).length).not.toEqual(0);
+    });
+
+    it("fetches workflow jobs if there were reruns", async () => {
+      expect((await subject.getWorkflowJobs(12, 3)).length).not.toEqual(0);
+    });
+  });
+
+  describe("getRunArtifacts", () => {
+    it("attemptes to fetch and load artifacts", async () => {
+      expect((await subject.getRunArtifacts()).length).not.toEqual(0);
+    });
+  });
+
+  describe("filterForRelevantJobDetails", () => {
+    beforeEach(() => {
+      subject.schema = {
+        jobs: {
+          some_job_name: {
+            name: "Some Verbose Job Name"
+          }
+        }
+      };
+    });
+
+    it("finds jobs that start with the job name", async () => {
+      const results = subject.filterForRelevantJobDetails("some_job_name", [
+        WorkflowJobFactory.generate({
+          name: "Some Verbose Job Name (matrix1)"
+        }),
+        WorkflowJobFactory.generate({
+          name: "Some Verbose Job Name (matrix2)"
+        }),
+        WorkflowJobFactory.generate({
+          name: "Some Verbose Job Name (matrix3)"
+        }),
+        WorkflowJobFactory.generate({
+          name: "Some Verbose Job Name (matrix4)"
+        }),
+        WorkflowJobFactory.generate({
+          name: "Some Verbose Job Name (matrix5)"
+        }),
+        WorkflowJobFactory.generate({ name: "Some other unrelated job" }),
+        WorkflowJobFactory.generate({ name: "another unrelated job" })
+      ]);
+      expect(results.length).toEqual(5);
+    });
+
+    it("does not find jobs that start with substrings", async () => {
+      const results = subject.filterForRelevantJobDetails("some_job_name", [
+        WorkflowJobFactory.generate({
+          name: "Some Verbose Job Name (matrix1)"
+        }),
+        WorkflowJobFactory.generate({
+          name: "Some Verbose Job Name (matrix2)"
+        }),
+        WorkflowJobFactory.generate({
+          name: "Some Verbose Job Name (matrix3)"
+        }),
+        WorkflowJobFactory.generate({
+          name: "Some Verbose Job Name (matrix4)"
+        }),
+        WorkflowJobFactory.generate({
+          name: "Some Verbose Job Name (matrix5)"
+        }),
+        WorkflowJobFactory.generate({ name: "Some other unrelated job" }),
+        WorkflowJobFactory.generate({ name: "another unrelated job" }),
+        WorkflowJobFactory.generate({
+          name: "Some Verbose Job Name But Not The Same (matrix1)"
+        }),
+        WorkflowJobFactory.generate({
+          name: "Some Verbose Job Name But Not The Same (matrix2)"
+        }),
+        WorkflowJobFactory.generate({
+          name: "Some Verbose Job Name But Not The Same (matrix3)"
+        })
+      ]);
+      expect(results.length).toEqual(5);
+    });
+  });
+
+  describe("getJobOutputs", () => {
+    let jobs: JobInfo[];
+    let artifacts: ArtifactInfo[];
+    
+    beforeEach(() => {
+      const mockArtifactResponse = ListWorkflowRunArtifactResponseFactory.generate();
+      artifacts = mockArtifactResponse.data.artifacts as ArtifactInfo[];
+      const mockJobsResponse = ListJobsForWorkflowRunFactory.generate();
+      jobs = mockJobsResponse.data.jobs as JobInfo[];
+
+      // Set 5 of the artifact names to match job ids.
+      jobs.slice(-5).forEach((job, i) => {
+        artifacts[i].name = job.id.toString();
+      });
+      jest.replaceProperty(subject, "artifacts", artifacts);
+      jest.spyOn(subject, "downloadArtifactFile").mockImplementation(async (id: number) => {
+        return `path/to/${id}`;
+      });
+      jest.spyOn(subject, "readOutputs").mockImplementation((filepath: string) => {
+        return `Results for ${filepath}`;
+      });
+    });
+
+    it("gets outputs for the specified jobs", async () => {
+      const result = await subject.getJobOutputs(jobs);
+      expect(Object.keys(result).length).toEqual(5);
+    });
+  });
+
+  describe("downloadArtifactFile", () => {
+    let zipperSpy: jest.SpyInstance;
+
+    beforeEach(() => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const Stream = require("stream");
+      const dataStream = fs.createReadStream(__filename);
+      jest.spyOn(dataStream, "pipe").mockImplementation((destination) => {
+        return destination;
+      });
+      jest.spyOn(fs, "createReadStream").mockImplementation(() => {
+        return dataStream;
+      });
+      zipperSpy = jest.spyOn(unzipper, "Extract");
+      zipperSpy.mockImplementation(() => {
+        const mocker = Stream.PassThrough;
+        mocker.promise = jest.fn();
+        return mocker;
+      });
+    });
+
+    it("tries to download the artifact as expected", async () => {
+      const downloadFileSpy = jest.spyOn(subject, "downloadFile").mockImplementation();
+      await subject.downloadArtifactFile(123);
+      expect(zipperSpy).toHaveBeenCalled();
+      expect(downloadFileSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe("readOutputs", () => {
+    it("reads the output file name from the given path", async () => {
+      jest.spyOn(fs, "readFileSync").mockImplementation((filePath) => {
+        return `{"someArbitrary": "JSON data", "from": "${filePath}"}`;
+      });
+
+      const result = subject.readOutputs("some/path/to/a/file");
+      expect(result).toEqual({"someArbitrary": "JSON data", "from": "some/path/to/a/file/outputFileName.txt"});
+    });
+  });
+
+  describe("downloadFile", () => {
+    it("downloads the file from the given URL", async () => {
+      const dataStream = fs.createReadStream(__filename);
+      mockAdapter.onAny().reply(200, dataStream);
+      await subject.downloadFile("https://somesite.org/some/path", "save/file/here.zip");
+      // and not result in error
+    });
+  });
+
+  describe("run", () => {
+    it("iterates over jobs by dependency and gets the data from GitHub", async () => {
+      
+      const getWorkflowSchemaSpy = jest.spyOn(subject, "getWorkflowSchema").mockImplementation(async () => workflowContent);
+      const getRunArtifactsSpy = jest.spyOn(subject, "getRunArtifacts").mockImplementation();
+      const getRelevantWorkflowJobsSpy = jest.spyOn(subject, "getRelevantWorkflowJobs").mockImplementation();
+      const getLastRanWorkflowJobsSpy = jest.spyOn(subject, "getLastRanWorkflowJobs").mockImplementation();
+      const getJobOutputsSpy = jest.spyOn(subject, "getJobOutputs").mockImplementation();
+      const coreSpy = jest.spyOn(core, "setOutput").mockImplementation();
+
+      await subject.run();
+
+      expect(getWorkflowSchemaSpy).toHaveBeenCalled();
+      expect(getRunArtifactsSpy).toHaveBeenCalled();
+      expect(getRelevantWorkflowJobsSpy).toHaveBeenCalled();
+      expect(getLastRanWorkflowJobsSpy).toHaveBeenCalled();
+      expect(getJobOutputsSpy).toHaveBeenCalled();
+
+      
+      workflowContent.jobs[subject.context.job].needs.forEach((needsName: string) => {
+        expect(coreSpy).toHaveBeenCalledWith(needsName, undefined);
+      });
+    });
   });
 });
--- src/consolidator.ts
+++ src/consolidator.ts
@@ -1,10 +1,17 @@
 import * as github from "@actions/github";
 import * as core from "@actions/core";
+import Axios from "axios";
 import * as tmp from "tmp";
+import YAML from "yaml";
 import { Context } from "@actions/github/lib/context";
 import { Octokit } from "@octokit/core";
 import { PaginateInterface } from "@octokit/plugin-paginate-rest";
 import { Api } from "@octokit/plugin-rest-endpoint-methods/dist-types/types";
+import * as fs from "fs";
+import * as unzipper from "unzipper";
+
+import { ArtifactInfo } from "./artifactInfo";
+import { JobInfo } from "./jobInfo";
 
 /**
  * Consolidate the output of all jobs that came prior to this job and return as the output of this job.
@@ -12,6 +19,9 @@ import { Api } from "@octokit/plugin-rest-endpoint-methods/dist-types/types";
 export class Consolidator {
   octokit: Octokit & Api & { paginate: PaginateInterface };
   context: Context;
+  artifacts: ArtifactInfo[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  schema: any;
 
   /**
    * Initialize clients and member variables.
@@ -19,6 +29,7 @@ export class Consolidator {
   constructor() {
     tmp.setGracefulCleanup(); // delete tmp files on process exit
 
+    this.artifacts = [];
     this.octokit = github.getOctokit(`${process.env.GITHUB_TOKEN}`);
     this.context = github.context;
     core.debug("Context:");
@@ -26,9 +37,260 @@ export class Consolidator {
   }
 
   /**
+   * Octokit query parameters that are used across multiple API requests.
+   */
+  commonQueryParams() {
+    return {
+      owner: this.context.payload.organization.login,
+      repo: `${this.context.payload.repository?.name}`
+    };
+  }
+
+  /**
    * Runtime entrypoint. Query for the last successful ran (not reran) jobs prior to this job and
    * return the content of the outputs JSON as an output of this job. Outputs of this job will have
    * the same key/name as the strings defined in the `needs` configuration.
    */
-  async run() {}
+  async run() {
+    this.schema = await this.getWorkflowSchema();
+    this.artifacts = await this.getRunArtifacts();
+    // const results: { [n: string]: JobInfo[] } = {};
+    for (const jobName of this.schema.jobs[this.context.job].needs) {
+      const currentWorkflowJobs = await this.getRelevantWorkflowJobs(
+        jobName,
+        this.context.runId
+      );
+      const lastRanWorkflows = await this.getLastRanWorkflowJobs(
+        jobName,
+        currentWorkflowJobs
+      );
+      const jobOutputs = await this.getJobOutputs(lastRanWorkflows);
+      core.setOutput(jobName, JSON.stringify(jobOutputs));
+    }
+  }
+
+  /**
+   * Get the GitHub Action Workflow schema for the currently running job. This will query for the
+   * YAML file of the current branch and return a data structure.
+   */
+  async getWorkflowSchema() {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const response: any = await this.octokit.rest.repos.getContent({
+      ...this.commonQueryParams(),
+      path: this.context.payload.workflow,
+      ref: this.context.payload.ref
+    });
+    core.debug("getContent");
+    core.debug(JSON.stringify(response));
+
+    const schema = YAML.parse(
+      Buffer.from(response.data.content, "base64").toString("utf8")
+    );
+
+    return schema;
+  }
+
+  /**
+   * Get jobs running within this workflow that are immediately preceding on this job, and have this
+   * job as a dependent. If a workflow has been reran, this will iteratively query previous runs
+   * until it can identify the job details that generated Artifacts.
+   */
+  async getLastRanWorkflowJobs(
+    jobName: string,
+    workflowJobs: JobInfo[]
+  ): Promise<JobInfo[]> {
+    if (workflowJobs.length == 0) return [];
+
+    // runAttempt should be the same across jobs
+    const runAttempt =
+      (workflowJobs.find((job) => job["run_attempt"]) || {})["run_attempt"] ||
+      1;
+
+    const jobsToReturn = workflowJobs.filter((job) => job.runner_id != 0) || [];
+    const jobsToRerun =
+      workflowJobs.filter(
+        (job) => job.runner_id == 0 && (job["run_attempt"] || 1) > 1
+      ) || [];
+
+    // return the relevant jobs immediately to avoid unneeded queries
+    if (jobsToRerun.length == 0 || !(runAttempt > 1)) return jobsToReturn;
+
+    // save the job names to filter by later
+    const reranJobNames = jobsToRerun.map((job) => job.name);
+    // query for the relevent jobs again, but from the previous run attempt
+    let moreJobs: JobInfo[] = await this.getRelevantWorkflowJobs(
+      jobName,
+      this.context.runId,
+      runAttempt - 1
+    );
+    // filter out the jobs that don't have the same name as the relevent ones from this run
+    moreJobs = moreJobs.filter((job) => reranJobNames.includes(job.name));
+    // return the jobs to return while recursing in case we need to look back farther in the run attempts
+    return jobsToReturn.concat(
+      await this.getLastRanWorkflowJobs(jobName, moreJobs)
+    );
+  }
+
+  /**
+   * Query for and filter jobs only relevent for the dependency relation.
+   */
+  async getRelevantWorkflowJobs(
+    jobName: string,
+    runId: number,
+    runAttempt: number | null = null
+  ): Promise<JobInfo[]> {
+    const workflowJobs = await this.getWorkflowJobs(runId, runAttempt);
+    return this.filterForRelevantJobDetails(jobName, workflowJobs);
+  }
+
+  /**
+   * Get all jobs running within this workflow. An optional attempt number can be passed.
+   */
+  async getWorkflowJobs(run_id: number, attempt_number: number | null = null) {
+    let workflowJobs = null;
+
+    if (attempt_number) {
+      workflowJobs =
+        await this.octokit.rest.actions.listJobsForWorkflowRunAttempt({
+          ...this.commonQueryParams(),
+          run_id,
+          attempt_number
+        });
+      core.debug("listJobsForWorkflowRunAttempt");
+      core.debug(JSON.stringify(workflowJobs));
+    } else {
+      workflowJobs = await this.octokit.rest.actions.listJobsForWorkflowRun({
+        ...this.commonQueryParams(),
+        run_id
+      });
+      core.debug("listJobsForWorkflowRun");
+      core.debug(JSON.stringify(workflowJobs));
+    }
+
+    return workflowJobs.data.jobs;
+  }
+
+  /**
+   * Get all artifacts associated with this run.
+   */
+  async getRunArtifacts(): Promise<ArtifactInfo[]> {
+    const response = await this.octokit.rest.actions.listWorkflowRunArtifacts({
+      ...this.commonQueryParams(),
+      run_id: this.context.runId
+    });
+    core.debug("listWorkflowRunArtifacts");
+    core.debug(JSON.stringify(response));
+
+    return response.data.artifacts;
+  }
+
+  /**
+   * Get the job details for any job that ran with that same definition. Matches the full name immediately followed by open parenthesis.
+   */
+  filterForRelevantJobDetails(
+    jobName: string,
+    workflowJobs: JobInfo[]
+  ): JobInfo[] {
+    const config = this.schema.jobs[jobName];
+    return workflowJobs.filter((job) => new RegExp(`^${config.name} \\(`).test(job.name));
+  }
+
+  /**
+   * Gather the outputs for the job runs and put them into an array.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async getJobOutputs(jobDetails: JobInfo[]): Promise<{ [k: string]: any }> {
+    // create a data structure with the job name and associated artifact
+    const jobArtifacts: { [k: string]: ArtifactInfo } = Object.fromEntries(
+      new Map(
+        jobDetails
+          .map((job) => [
+            job.name,
+            this.artifacts.find((a) => a.name == job.id.toString())
+          ])
+          .filter((e) => e[1] != undefined) as [string, ArtifactInfo][] // needed because the transcompiler can't tell we're filtering out undefined
+      )
+    );
+
+    core.debug(
+      `Found Artifacts (${JSON.stringify(
+        Object.values(jobArtifacts).map((a) => a.id)
+      )})`
+    );
+
+    // need to iterate to avoid defining async callbacks
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const jobResults: { [k: string]: any } = {};
+    for (const jobName of Object.keys(jobArtifacts)) {
+      const artifact = jobArtifacts[jobName];
+      const artifactPath = await this.downloadArtifactFile(artifact.id);
+      jobResults[jobName] = this.readOutputs(artifactPath);
+    }
+    core.debug(`Job Outputs: ${JSON.stringify(jobResults)}`);
+
+    // return the data structure as an array of objects
+    return jobResults;
+  }
+
+  /**
+   * Download and unpack an artifact to a temporary directory. Return the directory name.
+   */
+  async downloadArtifactFile(artifactId: number): Promise<string> {
+    const tmpFile = tmp.fileSync();
+    const tmpDir = tmp.dirSync();
+
+    // get the artifact download URL
+    // artifacts are stored as zip files
+    const response = await this.octokit.rest.actions.downloadArtifact({
+      ...this.commonQueryParams(),
+      artifact_id: artifactId,
+      archive_format: "zip"
+    });
+    core.debug("Artifact URL Info:");
+    core.debug(JSON.stringify(response));
+
+    // download the zip file for the artifact
+    await this.downloadFile(response.url, tmpFile.name);
+    core.debug(`Artifact Zip File Saved To: ${tmpFile.name}`);
+
+    // extract the artifact to a temporary directory
+    await fs
+      .createReadStream(tmpFile.name)
+      .pipe(unzipper.Extract({ path: tmpDir.name }))
+      .promise();
+    core.debug(
+      `Artifact Files Extracted To ${tmpDir.name}: ${JSON.stringify(
+        fs.readdirSync(tmpDir.name)
+      )}`
+    );
+
+    return tmpDir.name;
+  }
+
+  /**
+   * Read the outputs from the artifact directory path.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  readOutputs(artifactDirectoryPath: string): any {
+    const outputFilename = core.getInput("output_filename");
+    const readData = fs.readFileSync(`${artifactDirectoryPath}/${outputFilename}`, {
+      encoding: "utf8",
+      flag: "r"
+    });
+    core.debug(`Output File Contents: ${readData}`);
+    return JSON.parse(readData);
+  }
+
+  /**
+   * Download from a HTTPS endpoint and stream directly to file.
+   *
+   * Sourced from https://stackoverflow.com/questions/55374755/node-js-axios-download-file-stream-and-writefile
+   */
+  async downloadFile(fileUrl: string, outputLocationPath: string) {
+    const writer = fs.createWriteStream(outputLocationPath);
+    Axios.get(fileUrl, {responseType: "stream"}).then((response) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      return new Promise((_resolve, _reject) => response.data.pipe(writer));
+    });
+  }
 }
